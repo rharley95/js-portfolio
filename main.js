2272f49e39befef7c8e507c45a575e78b56c93f7
Romina Painter <romina.painter@stukent.com>
2019-10-26 20:44:15 -0600

    added easter egg

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function onCobalt() {
+	let bodyItem = document.body.style;
+	return bodyItem.backgroundColor = '#7EB2DD'; 
+}
+
+function onQueen() {
+	let bodyItem = document.body.style;
+	return bodyItem.backgroundColor = '#445E93';
+}
+
+function onGrey() {
+	let bodyItem = document.body.style;
+	return bodyItem.backgroundColor = '#DADDD8';
+}
+
+function thisText() {
+	document.getElementById('title').style.color = random();
+}
+
+function random() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
